006642e64e33f10c66a2163887c3eafcb4559efc
Damian Maciejewski <damian.maciej0@gmail.com>
2023-03-24 15:36:24 +0100

    Added responsiveness, editing note in progress

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const editBtn = document.querySelector(`.edit`);
+const saveEditBtn = document.querySelector(`.saveEdit`);
+const cancelEditBtn = document.querySelector(`.cancelEdit`);
+const textareaEditor = document.querySelector(`#textEditor`);
+const noteEditPanel = document.querySelector(`.note-edit-panel`);
+
 const addBtn = document.querySelector(`.add`);
 const saveBtn = document.querySelector(`.save`);
 const cancelBtn = document.querySelector(`.cancel`);
-const delateNote = document.getElementsByClassName(`delate-note`);
+const delateNote = document.getElementsByClassName(`.delate-note`);
 const btnDelateAll = document.querySelector(`.delate-all`);
 
 const noteArea = document.querySelector(`.note-area`);
@@ -23,7 +29,6 @@ const closePanel = () => {
     textarea.value = ``;
     noteCategory.selectedIndex = 0;
 }
-
 const addNote = () => {
     if (textarea.value != `` && noteCategory.options[noteCategory.selectedIndex].value != `0`){
         createNote();
@@ -40,12 +45,16 @@ const createNote = () => {
 
     newNote.innerHTML = `
     <div class="note-header">
-                <h3 class="note-title">${selectedValue}</h3>
-                <button class="delate-note" onclick="deleteNote(${cardID})"><i class="fas fa-times icon"></i></button>
-            </div>
-            <div class="note-body">
-                ${textarea.value}
-            </div>
+        <h3 class="note-title">${selectedValue}</h3>
+    
+        <button class="editBtn" onclick="openEditPanel(${cardID})"><i class="fa-solid fa-gear"></i></i></button>
+
+        <button class="delate-note" onclick="deleteNote(${cardID})"><i class="fa-solid fa-trash-can"></i></button>
+    </div>
+    
+    <div class="note-body">
+        ${textarea.value}
+    </div>
     `
 
     noteArea.appendChild(newNote);
@@ -63,15 +72,36 @@ const selectValue = () => {
 const checkColor = note => {
 
     switch(selectedValue) {
-        case `Zakupy`:
+        case `Shopping`:
             note.style.backgroundColor = `rgb(72,255,0)`;
             break;
-        case `Praca`:
+        case `Work`:
             note.style.backgroundColor = `rgb(255,243,0)`;
             break;
-        case `Inne`:
+        case `Other`:
             note.style.backgroundColor = `rgb(0,170,255)`;
             break;
+    }
+}
+
+const openEditPanel = id => {
+    noteEditPanel.style.display = `flex`;
+    const element = document.getElementById(id);
+    textareaEditor.value = element.lastElementChild.textContent;    
+}
+
+const closeEditPanel = () => {
+    noteEditPanel.style.display = `none`;
+    error.style.visibility =`hidden`;
+    textareaEditor.value = ``;
+}
+
+const saveEditPanel = () => {
+    if (textareaEditor.value != ``){
+        // editNote();
+        error.style.visibility =`hidden`;
+    } else {
+        error.style.visibility =`visible`;
     }
 }
 
@@ -87,4 +117,8 @@ const deleteAllNotes = () => {
 addBtn.addEventListener(`click`, openPanel);
 cancelBtn.addEventListener(`click`, closePanel);
 saveBtn.addEventListener(`click`, addNote);
+
+saveEditBtn.addEventListener(`click`, saveEditPanel);
+cancelEditBtn.addEventListener(`click`, closeEditPanel);
+
 btnDelateAll.addEventListener(`click`, deleteAllNotes);
